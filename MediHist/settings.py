d181b96e5c34ad1dd70f9b41f1907ad7610ae75d
Gurini <gurinidev@gmail.com>
2026-02-07 23:16:51 +0100

    Created and modfied templates and urls

--- MediHist/settings.py
+++ MediHist/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Users',
+    'Users', # Custom users app
+    'records', #Medical records management app
 ]
 
 MIDDLEWARE = [
--- MediHist/urls.py
+++ MediHist/urls.py
@@ -19,5 +19,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Users.urls')),
+    path('users/', include('Users.urls')),
+    path('records/', include('records.urls')),
 ]
+
+
+#Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
