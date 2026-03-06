237b779835c5e81d21ab1aa58c442da8463302e5
Gurini <gurinidev@gmail.com>
2026-03-06 06:59:23 +0100

    Minor fix

--- MediHist/urls.py
+++ MediHist/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('Users.urls')),
+    path('', include('Users.urls')),
     path('records/', include('records.urls')),
 ]
 
