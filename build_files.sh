256ba65b14d66278b50600b6b3ee8b52c81ebb3d
Gurini <gurinidev@gmail.com>
2026-03-06 06:41:05 +0100

    Minor fix

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
 pip install -r requirements.txt --break-system-packages --root-user-action=ignore
 python manage.py collectstatic --noinput
+
+mkdir -p staticfiles_build/static
+cp -r staticfiles/* staticfiles_build/static/
